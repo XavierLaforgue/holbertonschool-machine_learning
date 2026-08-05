17b21f7685cba716c706a8e22aee74725d6d3fa1
Xavier Laforgue <xavierjimmys@gmail.com>
2026-08-05 19:23:11 +0200

    Add solution to task 12: bracing the elements

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Define function np_elementwise"""
+
+
+def np_elementwise(mat1, mat2):
+    """Perform element-wise arithmetics.
+    Perform addition, subtraction, multiplication, and division"""
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
