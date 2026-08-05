8bfe08d326c752e2d31b1559189cd656ea686d8d
Xavier Laforgue <xavierjimmys@gmail.com>
2026-08-05 10:50:00 +0200

    Add solution to task 2: size me please

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Define function matrix_shape
+
+def matrix_shape(matrix: list[int] | list[list]) -> list[int]:
+    """Calculate the shape of the input matrix"""
+    shape = [len(matrix)]
+    if isinstance(matrix[0], int):
+        return shape
+    shape.extend(matrix_shape(matrix[0]))
+    return shape
