7b1f14f3ef20b2ecca8e3622c29b44a8b278a56d
Xavier Laforgue <xavierjimmys@gmail.com>
2026-08-06 03:01:05 +0200

    Add function docstring

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+"""Define function line."""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
-    """Plot a line graph"""
+    """Plot a line graph."""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
