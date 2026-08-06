559331dfa8178cc62285bb73b9dac4b72006cbca
Xavier Laforgue <xavierjimmys@gmail.com>
2026-08-06 19:10:08 +0200

    Add solution to plotting, task 4: frequency

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Define function frequency."""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """Plot histogram of student scores for a project."""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+    bins = np.arange(0, 101, 10)
+    plt.hist(student_grades, bins=bins, edgecolor='k')
+    plt.xlim(0, 100)
+    plt.xticks(np.arange(0, 101, 10))
+    plt.ylim(0, 30)
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+    plt.show()
