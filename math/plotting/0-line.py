8e6fa893d4d0f0267d3b62e96a3784ced3aaa0c0
Xavier Laforgue <xavierjimmys@gmail.com>
2026-08-06 02:44:08 +0200

    Fix x-axis limits

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -9,6 +9,6 @@ def line():
     plt.figure(figsize=(6.4, 4.8))
 
     x = np.arange(0, 11)
-    plt.plot(x, y, color='red')
-    plt.xlim(right=10)
+    plt.plot(x, y, linestyle='-', color='red')
+    plt.xlim(0, 10)
     plt.show()
