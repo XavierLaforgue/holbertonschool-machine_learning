c87b26ce6614e910ecaac0d529844adbd7b7dbb6
Xavier Laforgue <xavierjimmys@gmail.com>
2026-08-07 03:16:40 +0200

    Use element-wise exponentiation on an ndarray to avoid loops

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Define function summation_i_squared."""
+import numpy as np
 
 
 def summation_i_squared(n):
-    if not isinstance(n, int): return None
-    return sum(i**2 for i in range(1, n+1))
+    if not isinstance(n, int) or n < 1: return None
+    return np.sum(np.arange(1, n + 1, 1)**2)
