57e99bb8f9405f4b4f2f2856c5613496d3415bc2
Xavier Laforgue <xavierjimmys@gmail.com>
2026-08-07 03:22:42 +0200

    Fix pycodestyle and pydocstyle

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,5 +4,7 @@ import numpy as np
 
 
 def summation_i_squared(n):
-    if not isinstance(n, int) or n < 1: return None
+    r"""Calculate \\sigma_{i=1}^n i^2."""
+    if not isinstance(n, int) or n < 1:
+        return None
     return np.sum(np.arange(1, n + 1, 1)**2)
