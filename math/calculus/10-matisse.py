4fee7ffab8c997590d93034d9fc0b1389ec6491d
Xavier Laforgue <xavierjimmys@gmail.com>
2026-08-07 03:54:42 +0200

    Add solution to calculus, task 10: derive happiness in oneself from a good day's work

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Define function poly_derivative."""
+import numpy as np
+
+
+def poly_derivative(poly):
+    """Calculate the derivative of a polynomial."""
+    if not isinstance(poly, list)\
+            or not poly\
+            or any(not isinstance(elem, int) for elem in poly):
+        return None
+    if len(poly) == 1:
+        return [0]
+    deriv = np.asarray(poly[1:]) * np.arange(1, len(poly))
+    # deriv = []
+    # for idx, val in enumerate(poly):
+    #     if idx == 0:
+    #         continue
+    #     deriv.append(idx * val)
+    return deriv
